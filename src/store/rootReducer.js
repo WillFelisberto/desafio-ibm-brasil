d7b3daf8b0169ccc08af10e5b4e659e7875ac0c3
Willian Souza <willian_aru_@hotmail.com>
2022-02-19 16:19:41 -0300

    First Commit > Project Created > Adjusted project structure > Added libs > Added Redux framework

---
+++ src/store/rootReducer.js
@@ -0,0 +1,13 @@
+// import starred from './starred/reducer';
+// import repositories from './repositories/reducer';
+// import user from './user/reducer';
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+	// starred,
+	// repositories,
+	// user,
+});
+
+export default rootReducer;
---
+++ src/store/rootSagas.js
@@ -0,0 +1,11 @@
+import { all, fork } from 'redux-saga/effects';
+
+// import * as starredSagas from './starred/sagas';
+// import * as repositoriesSagas from './repositories/sagas';
+// import * as userSagas from './user/sagas';
+
+export default function* rootSaga() {
+	// yield all([...Object.values(starredSagas)].map(fork));
+	// yield all([...Object.values(repositoriesSagas)].map(fork));
+	// yield all([...Object.values(userSagas)].map(fork));
+}
